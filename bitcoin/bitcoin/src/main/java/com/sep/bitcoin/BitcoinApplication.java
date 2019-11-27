0813662751cd883acbeb16db1f573c73356839cc
nikolaslijepcevic <miljancabrilo@yahoo.com>
2019-11-27 23:32:09 +0100

    HTTPS konfigurisan

--- bitcoin/bitcoin/src/main/java/com/sep/bitcoin/BitcoinApplication.java
+++ bitcoin/bitcoin/src/main/java/com/sep/bitcoin/BitcoinApplication.java
@@ -1,14 +1,41 @@
 package com.sep.bitcoin;
 
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.shared.transport.jersey.EurekaJerseyClientImpl.EurekaJerseyClientBuilder;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class BitcoinApplication {
+	
+	@Bean
+	public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs() throws NoSuchAlgorithmException {
+		DiscoveryClient.DiscoveryClientOptionalArgs args = new DiscoveryClient.DiscoveryClientOptionalArgs();
+		System.setProperty("javax.net.ssl.keyStore", "src/main/resources/bitcoinkeystore.jks");
+		System.setProperty("javax.net.ssl.keyStorePassword", "bitcoinpass");
+		System.setProperty("javax.net.ssl.trustStore", "src/main/resources/bitcoinkeystore.jks");
+		System.setProperty("javax.net.ssl.trustStorePassword", "bitcoinpass");
+		EurekaJerseyClientBuilder builder = new EurekaJerseyClientBuilder();
+		builder.withClientName("bitcoin");
+		builder.withSystemSSLConfiguration();
+		builder.withMaxTotalConnections(10);
+		builder.withMaxConnectionsPerHost(10);
+		args.setEurekaJerseyClient(builder.build());
+		return args;
+	}
 
 	public static void main(String[] args) {
+		System.setProperty("KEY_STORE_CLASSPATH", "src/main/resources/bitcoinkeystore.jks");
+		System.setProperty("KEY_STORE_PASSWORD", "bitcoinpass");
+		System.setProperty("KEY_ALIAS", "bitcoin");
+		System.setProperty("EUREKA_INSTANCE_HOSTNAME", "localhost");
+		System.setProperty("CLIENT_SERVICEURL_DEFAULTZONE", "https://localhost:8761/eureka/");
 		SpringApplication.run(BitcoinApplication.class, args);
 	}
 
--- zuul/zuul/src/main/java/com/sep/zuul/ZuulApplication.java
+++ zuul/zuul/src/main/java/com/sep/zuul/ZuulApplication.java
@@ -4,13 +4,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
 public class ZuulApplication {
+	
+	@Bean
+	 public RestTemplate template() throws Exception{
+		 RestTemplate template = new RestTemplate();
+		 return template;
+	 }
 
 	public static void main(String[] args) {
+		System.setProperty("KEY_STORE_CLASSPATH", "src/main/resources/zuulkeystore.jks");
+		System.setProperty("KEY_STORE_PASSWORD", "zuulpass");
+		System.setProperty("KEY_ALIAS", "zuul");
+		System.setProperty("EUREKA_INSTANCE_HOSTNAME", "localhost");
+		System.setProperty("CLIENT_SERVICEURL_DEFAULTZONE", "https://localhost:8761/eureka/");
+		System.setProperty("TRUST_STORE_CLASSPATH", "zuulkeystore.jks");
+		System.setProperty("TRUST_STORE_PASSWORD", "classpath:zuulpass");
 		SpringApplication.run(ZuulApplication.class, args);
 	}
 
